30f46d23642cae57a674e5dc873a9deb2f8eeb5e
Joshua Bracks <joshabracks@gmail.com>
2024-08-30 23:06:41 -0700

    pause

--- gameplay/level/level_controller.gd
+++ gameplay/level/level_controller.gd
@@ -6,11 +6,21 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for child in get_children():
+		child.process_mode = PROCESS_MODE_PAUSABLE
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		var is_paused = get_tree().paused
+		if is_paused:
+			get_tree().paused = false
+		else:
+			get_tree().paused = true
+	if get_tree().paused:
+		return
 	if Global.restart:
 		Global.game_over = false
 		Global.restart = false
